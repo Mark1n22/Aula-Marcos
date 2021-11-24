8f07fd2c4d439ad89aff96e730dc6f1f2560d165
Mark1n22 <marcos_santos20@estudante.sesisenai.org.br>
2021-11-24 16:53:24 -0200

    Primeiro commit

---
+++ DiasDaSemana.js
@@ -0,0 +1,17 @@
+// const carros = ["Ferrari", "Relampago", 237]
+
+// console.log(carros[0]);
+
+//     carros [2] = "Onix"
+//     console.log(carros);
+
+
+const diasdasemana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta","Sabado"]
+
+console.log(diasdasemana[0]);
+console.log(diasdasemana[1]);
+console.log(diasdasemana[2]);
+console.log(diasdasemana[3]);
+console.log(diasdasemana[4]);
+console.log(diasdasemana[5]);
+console.log(diasdasemana[6]);
